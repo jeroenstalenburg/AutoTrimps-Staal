8b4ff8080173d51f28564517689d8464cee8bc90
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2021-10-25 16:05:46 +0200

    better debug to console.log

--- modules/perks.js
+++ modules/perks.js
@@ -1331,8 +1331,8 @@ RAutoPerks.VariablePerk = function(name, base, compounding, value, baseIncrease,
     this.exprate = 1.3;
     if (name === "championism") {
         this.exprate = 5;
-        debug("This should be once")
     }
+    console.log(name + ': ' + this.exprate)
     this.fixed = false;
     this.compounding = compounding;
     this.updatedValue = -1;
