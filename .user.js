7b1c4be6118af8e5b76232d62135cb03de977eab
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2021-10-25 13:50:26 +0200

    Updated to own link

--- .user.js
+++ .user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoTrimps-Staal
 // @version      1.0-Staal
-// @namespace    https://github.com/jeroenstalenburg/AutoTrimps-Staal
-// @updateURL    https://github.com/jeroenstalenburg/AutoTrimps-Staal/.user.js
+// @namespace    https://jeroenstalenburg.github.io/AutoTrimps-Staal
+// @updateURL    https://jeroenstalenburg.github.io/AutoTrimps-Staal/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Staalli2
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *https://github.com/jeroenstalenburg/AutoTrimps-Staal*
+// @connect      *jeroenstalenburg.github.io/AutoTrimps-Staal*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        GM_xmlhttpRequest 
@@ -16,7 +16,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Staal';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://raw.githubusercontent.com/jeroenstalenburg/AutoTrimps-Staal/main/AutoTrimps2.js?token=AKLVYK2K2L6JNAM32VA7MFLBP7CPE';
-script.setAttribute('crossorigin',"use-credentials");
-// script.setAttribute('crossorigin',"anonymous");
+script.src = 'https://jeroenstalenburg.github.io/AutoTrimps-Staal/AutoTrimps2.js';
+//script.setAttribute('crossorigin',"use-credentials");
+script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
